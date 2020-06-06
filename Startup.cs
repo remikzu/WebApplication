c584a33d05615d7eb580af17a766ee59a80256e5
Remigiusz Zudzin <remik.zudzin@gmail.com>
2020-06-06 14:43:31 +0200

    Configured .NET Core Solution so it opens index.html

--- Startup.cs
+++ Startup.cs
@@ -12,12 +12,6 @@ namespace WebApplication
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -26,15 +20,8 @@ namespace WebApplication
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
         }
     }
 }
